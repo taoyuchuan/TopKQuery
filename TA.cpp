26c3f941768448a2f376292f14b272b1f8242eb7
wenqiwang <wenqiwang1993@gmail.com>
2017-04-13 14:37:01 -0400

    add comments

--- TA.cpp
+++ TA.cpp
@@ -1,3 +1,7 @@
+/**************************************************************
+Implementation for all member functions for class FA algorithm
+**************************************************************/
+
 #include <iostream>
 #include <queue>
 #include <iomanip>
@@ -5,25 +9,35 @@
 
 using namespace std;
 
+
+// default constructor
+// calls the base class default constructor to initialize data set
+// default value for top K is defined as 3
 TA::TA():TopKAlgorithm()
 {
 }
 
+// constructor
+// call base class constructor
+// data set and topK is provided as the parameter
 TA::TA(const DataSet& rhs_dataSet, const size_t& rhs_topK)
   :TopKAlgorithm(rhs_dataSet, rhs_topK)
 { 
 }
 
+//copy constructor
 TA::TA(const TA& rhs):TopKAlgorithm(rhs)
 {
   topKQuery = rhs.topKQuery;
 }
 
+//move constructor
 TA::TA(TA&& rhs):TopKAlgorithm(std::move(rhs))
 {
   topKQuery = std::move(rhs.topKQuery);
 }
 
+//copy assignemnt constructor
 TA& TA::operator=(const TA& rhs)
 {
   if(this != &rhs)
@@ -34,6 +48,7 @@ TA& TA::operator=(const TA& rhs)
   return *this;
 }
 
+//move assignment consctor
 TA& TA::operator=(TA&& rhs)
 {
   if(this != &rhs)
@@ -45,14 +60,38 @@ TA& TA::operator=(TA&& rhs)
   return *this;
 }
 
+//destructor
 TA::~TA()
 {
 }
 
+// return the times of data items seen in all lists
+unordered_map<size_t, size_t> TA::getIdTimes() const
+{
+  return idTimes;
+}
 
+// set the value of idTimes
+void TA::setIdTimes(const unordered_map<size_t, size_t>& rhs_idTimes)
+{
+  if(!rhs_idTimes.empty())
+    {
+      idTimes = rhs_idTimes;
+    }
+  else
+    {
+      cout <<"Invalid idTimes value" << endl;
+      return;
+    }
+}
+
+// the core function to perform the FA algorithm for top-k query
+// the top-K query result will be stored in topKQuery vector
+// the result will contain the id and overall score of top-k data item
 void TA::TASolution()
 {
   size_t threshold = 0;
+  //check the input value
   if(dataSet.getDataSize() <= 0 || topK <= 0)
     {
       cout << "Invalid data set or invalid topK value" << endl;
@@ -60,17 +99,23 @@ void TA::TASolution()
     }
   vector<vector<DataItem>> allLists = dataSet.getLists();
   priority_queue<DataOverallScore, vector<DataOverallScore>, greater<DataOverallScore>> myPriorityQueue;
+  //do the sequencial accesses to all the lists
   for (int i = 0; i < dataSet.getDataSize(); i++)
   {
+     //do the parallel accesses to all the lists
      for (int j =0; j < dataSet.getListSize(); j++)
      {
-	   sequenceAccess += 1;
+      sequenceAccess += 1;
        DataItem tempDataItem = allLists[j][i];
        size_t tempId = tempDataItem.getId();
        threshold += tempDataItem.getScore();
+      //access all the other lists to get all the local scores of this data item
        vector<int> allScore = dataSet.checkScore(tempId);
+       //if this data item is first time seen
 	   if(idTimes.count(tempId) == 0)
 	   {
+	//calculate its overall score and compare with the "top" in PriorityQueue
+	//to decide if push or not
          DataOverallScore temp;
 		 idTimes[tempId] = 1;
          temp.setId(tempId);
@@ -89,10 +134,13 @@ void TA::TASolution()
 	       }
 	   }
      }
+     //stop when priorityQueue is full and the smallest value in prioirtyQueue is smaller than or equal to the threashold
      if( myPriorityQueue.size() == topK && myPriorityQueue.top().getOverallScore() >= threshold)
        break;
      threshold = 0;
   }
+
+ // store the result to topKQuery
   topKQuery.resize(topK);
 
   size_t index = topK - 1;
@@ -108,9 +156,11 @@ void TA::TASolution()
   executionCost = sequenceAccess + cost * randomAccess;
 }
 
+//clear the TA class
 void TA::clear()
 {
   TopKAlgorithm::clear();
   topKQuery.clear();
+  idTimes.clear();
 }
 
--- TA.h
+++ TA.h
@@ -1,3 +1,12 @@
+/***************************************************************************
+Defines the header of TA class
+The class inherits from TopKAlgorithm class
+Additional private data members, topKQuery and idTimes are added
+the topK result will be stored in a vector topKQuery
+the idTimes records the seen data information   
+The actual implementation is in file FA.cpp
+***************************************************************************/
+
 #ifndef TA_H
 #define TA_H
 
@@ -14,19 +23,31 @@ using namespace std;
 class TA: public TopKAlgorithm
 {
   public:
+    //constructor
     TA();
     TA(const DataSet &rhs_dataSet, const size_t & rhs_topk);
     TA(const TA& rhs);
     TA(TA&& rhs);
     TA& operator=(const TA& rhs);
     TA& operator=(TA&& rhs);
+    //destructor
     ~TA();
+    
+    //get functions
+    unordered_map<size_t, size_t> getIdTimes() const;
+    
+    //set functions
+    void setIdTimes(const unordered_map<size_t, size_t>& rhs_idTimes);
 
-    void TASolution();    
+    // function that will implement FA algorithm
+    void TASolution();
+
+    // helper function to clear the class data member    
     void clear();
 
   private:
-     unordered_map<size_t, size_t> idTimes;
+     //private data member
+     unordered_map<size_t, size_t> idTimes;   //// store the number of times data item show up in all lists
 };
 
 #endif
